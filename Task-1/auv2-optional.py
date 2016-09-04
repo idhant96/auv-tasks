bdfc303e760cbc46bdf61f4993e6992a43775568
coldplayer <idhant123@gmail.com>
2016-09-05 04:02:53 +0530

    new

--- Task-1/auv2-optional.py
+++ Task-1/auv2-optional.py
@@ -32,11 +32,31 @@ def cont(clr,im):
    cv2.circle(img, (cx, cy), 1, (255, 255, 255), -1)
    cv2.putText(img, clr, (cx - 20, cy - 20),
                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-   cv2.imshow('control',img)
+   cv2.imshow('image',img)
 
 
 def nothing(x):
    pass
+
+iLowH = 0
+iHighH = 255
+iLowS = 0
+iHighS = 255
+iLowV = 0
+iHighV = 255
+
+
+# Creating track bar
+
+cv2.createTrackbar('LowH', 'control', iLowH, 255,nothing)
+cv2.createTrackbar("HighH", "control",iHighH, 255,nothing)
+
+cv2.createTrackbar("LowS", "control",iLowS, 255,nothing)
+cv2.createTrackbar("HighS", "control",iHighS, 255,nothing)
+
+cv2.createTrackbar("LowV", "control", iLowV, 255,nothing)
+cv2.createTrackbar("HighV", "control", iHighV, 255,nothing)
+
 
 switchG = '0 : green \n1 : set'
 cv2.createTrackbar(switchG, 'control',0,1,nothing)
@@ -47,39 +67,51 @@ cv2.createTrackbar(switchR, 'control',0,1,nothing)
 switchY = '0 : Yellow   \n1 : Set '
 cv2.createTrackbar(switchY, 'control',0,1,nothing)
 
-#hardcoding the hsv color spaces for ease
-low_yellow=np.array([22,118,200])
-high_yellow=np.array([38,255,255])
-low_red=np.array([0,140,177])
-high_red=np.array([21,255,255])
-low_green=np.array([69,160,101])
-high_green=np.array([82,255,255])
+
 
 while True:
-
+    cv2.imshow('contro',result)
     k = cv2.waitKey(30) & 0xFF
     if k == 27 :
         cv2.destroyAllWindows()
         break
 
+    # get current positions of four trackbars
+    iLowH = cv2.getTrackbarPos('LowH','control')
+    iHighH = cv2.getTrackbarPos('HighH','control')
+
+    iLowS = cv2.getTrackbarPos('LowS','control')
+    iHighS = cv2.getTrackbarPos('HighS', 'control')
+
+    iLowV = cv2.getTrackbarPos('LowV', 'control')
+    iHighV = cv2.getTrackbarPos('HighV', 'control')
+
     sR = cv2.getTrackbarPos(switchR, 'control')
     sG = cv2.getTrackbarPos(switchG, 'control')
     sY = cv2.getTrackbarPos(switchY, 'control')
+
+    lower = np.array([iLowH, iLowS, iLowV])
+    upper = np.array([iHighH, iHighS, iHighV])
+
+    mask = cv2.inRange(hsv, lower, upper)
+    cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel, mask)
+    result = cv2.bitwise_and(hsv, hsv, mask=mask)
+
     if sR == 1:
-        mask = cv2.inRange(hsv, low_red, high_red)
+        mask = cv2.inRange(hsv, lower, upper)
         cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel, mask)
         result = cv2.bitwise_and(hsv, hsv, mask=mask)
         _, _, v = cv2.split(result)
         cont("ORANGE", v)
 
     if sG == 1:
-        mask = cv2.inRange(hsv, low_green, high_green)
+        mask = cv2.inRange(hsv, lower , upper)
         cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel, mask)
         result = cv2.bitwise_and(hsv, hsv, mask=mask)
         _, _, v = cv2.split(result)
         cont("green", v)
     if sY == 1:
-       mask = cv2.inRange(hsv, low_yellow, high_yellow )
+       mask = cv2.inRange(hsv, lower, upper )
        cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel, mask)
        result = cv2.bitwise_and(hsv, hsv, mask=mask)
        _,_,v=cv2.split(result)
